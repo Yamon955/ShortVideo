d3276e06e266f7b84cdcf292924d1ab9d8237904
yamonwang <yamonwang@tencent.com>
2025-01-24 14:38:56 +0800

    feat:add redis

--- video/main.go
+++ video/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/Yamon955/ShortVideo/protocol/video/pb"
+	"github.com/Yamon955/ShortVideo/video/entity/conf"
+	"github.com/Yamon955/ShortVideo/video/repo/redis"
 	"trpc.group/trpc-go/trpc-go"
 	"trpc.group/trpc-go/trpc-go/codec"
 )
@@ -9,6 +11,13 @@ import (
 func init() {
 	// 对于 Content-Type=multipart/form-data 的请求无需反序列化，因此注册对应的反序列化处理对象，重写其 Unmarshal 方法
 	codec.RegisterSerializer(SerializationTypeFormData, &FormDataSerialization{})
+
+	if err := conf.Init(); err != nil {
+		panic(err)
+	}
+	if err := redis.Init(); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
