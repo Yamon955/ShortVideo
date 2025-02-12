a473bc4ab14cff14fceda0ea59ba5eb51abb82eb
yamonwang <yamonwang@tencent.com>
2025-02-12 21:25:43 +0800

    feat:用户新增redis保存tag信息

--- user/entity/def/def.go
+++ user/entity/def/def.go
@@ -9,4 +9,10 @@ const (
 	MIN_UID = 1 // 最小的合法 uid
 
 	MySQLErrCode_UsernameIsDuplicate = 1062 // mysql 唯一键约束
+
+	UserTagKeyPrefix      = "TAG:UID:"       // 存储 uid 的 tag 值的 key 前缀
+	BloomFilter1Keysuffix = "_BloomFilter_1" // 用户布隆过滤器1 key 后缀
+	BloomFilter2Keysuffix = "_BloomFilter_2" // 用户布隆过滤器2 key 后缀
+	BloomFilterErrRate    = 0.01             // 布隆过滤器误差率
+	BloomFilterCapacity   = 1000             // 布隆过滤器容量
 )
--- user/main.go
+++ user/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Yamon955/ShortVideo/protocol/user/pb"
 	"github.com/Yamon955/ShortVideo/user/entity/conf"
 	"github.com/Yamon955/ShortVideo/user/repo/mysql"
+	"github.com/Yamon955/ShortVideo/user/repo/redis"
 	"trpc.group/trpc-go/trpc-go"
 )
 
@@ -15,6 +16,9 @@ func main() {
 		panic(err)
 	}
 	if err := mysql.Init(); err != nil {
+		panic(err)
+	}
+	if err := redis.Init(); err != nil {
 		panic(err)
 	}
 
